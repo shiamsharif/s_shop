7391d0c9d1b44ece3761751726de5d1a4f91605a
shiamsharif <shiam.sharif.07@gmail.com>
2025-07-20 17:10:59 +0600

    Update: Order models

--- shop/models.py
+++ shop/models.py
@@ -116,9 +116,10 @@ class OrderItem(models.Model):
     order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
     product = models.ForeignKey(Product, related_name='order_items', on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return f'{self.quantity} of {self.product.name} in Order {self.order.id}'
 
     def get_cost(self):
-        return self.product.price * self.quantity
+        return self.price * self.quantity
