ba92369fc9cfd89e0be1066ab98ce922245a4d2b
shiamsharif <shiam.sharif.07@gmail.com>
2025-07-21 14:27:35 +0600

    ADD: PRoducta and cart admin done

--- shop/admin.py
+++ shop/admin.py
@@ -10,3 +10,35 @@ class CategoryAdmin(admin.ModelAdmin):
     search_fields = ('name',)
     ordering = ('name',)
     show_facets = admin.ShowFacets.ALWAYS
+
+class RatingInline(admin.TabularInline):
+    model = Rating
+    extra = 0
+    readonly_fields = ('user', 'rating','comment' , 'created')
+    #show_facets = admin.ShowFacets.ALWAYS
+    
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'stock', 'available', 'created', 'updated')
+    list_filter = ('category', 'available', 'created', 'updated')
+    list_editable = ('price', 'stock', 'available')
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ('name', 'description')
+    ordering = ('-created',)
+    inlines = [RatingInline]
+    show_facets = admin.ShowFacets.ALWAYS
+    
+
+class CartItemInline(admin.TabularInline):
+    model = CartItem
+    extra = 0
+    
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('user', 'created_at', 'updated_at',)
+    list_filter = ('user', 'created_at', 'updated_at')
+    search_fields = ('user__username',)
+    ordering = ('-created_at',)
+    inlines = [CartItemInline]
+    show_facets = admin.ShowFacets.ALWAYS
