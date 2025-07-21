f1ccc845df19b73b529eee9319cd3e885e4a00eb
shiamsharif <shiam.sharif.07@gmail.com>
2025-07-21 14:12:05 +0600

    ADD: Category admin

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Category, Product, Rating, Cart, Order, CartItem, OrderItem
 
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    list_filter = ('name',)
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ('name',)
+    ordering = ('name',)
+    show_facets = admin.ShowFacets.ALWAYS
