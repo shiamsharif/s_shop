a9fa8836cd993382ea726c5315dbed61d9f8ba68
shiamsharif <shiam.sharif.07@gmail.com>
2025-07-22 10:17:46 +0600

    ADD: Home and Details View

--- shop/views.py
+++ shop/views.py
@@ -4,9 +4,11 @@ from django.contrib.auth.decorators import login_required
 from .models import Product, Category, Cart, Order, CartItem, OrderItem, Rating
 from .forms import RegistrationForm, RatingForm, CheckoutForm
 from django.contrib import messages
+from django.db.models import Min, Max, Count, Avg
+form django.db.models import Q
 
 
-# Create your views here.
+# ---------------- Part 1 ----------------
 def login_view(request):
     if request.method == 'POST':
         username = request.POST.get('username')
@@ -39,3 +41,54 @@ def logout_view(request):
     logout(request)
     messages.success(request, 'You have been logged out.')
     return redirect('shop:login')
+
+
+def home(request):
+    featured_products = Product.objects.filter(available=True).order_by('-created_at')[:8]
+    categories = Category.objects.all()
+    
+    
+    return render(request, 'shop/home.html', {
+        'featured_products': featured_products,
+        'categories': categories
+    })
+    
+    
+def product_detail(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    
+    
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+        
+    min_price = products.aggregate(Min('price'))['price__min']
+    max_price = products.aggregate(Max('price'))['price__max']
+    
+    if request.GET.get('min_price'):
+        products = products.filter(price__gte=request.GET.get('min_price'))
+        
+    if request.GET.get('max_price'):
+        products = products.filter(price__lte=request.GET.get('max_price'))
+        
+    if request.GET.get('rating'):
+        min_rating = int(request.GET.get('rating'))
+        products = products.annotate(avg_rating=Avg('ratings__rating')).filter(avg_rating__gte=min_rating)
+        
+    if request.GET.get('search'):
+        query = request.GET.get('search')
+        products = Product.objects.filter(
+            Q(name__icontains=query) |
+            Q(description__icontains=query) |
+            Q(category__icontains=query)
+        )
+        
+    return render(request, 'shop/product_detail.html', {
+        'category': category,
+        'categories': categories,
+        'products': products,
+        'min_price': min_price,
+        'max_price': max_price,
+    })
